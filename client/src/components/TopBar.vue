55149516f8dac2f68e7087ef063edf77c97c2294
Hayk Gevorgyan <g.hayk.111@gmail.com>
2025-06-01 04:11:15 +0400

    added copy button to top bar

--- client/src/components/TopBar.vue
+++ client/src/components/TopBar.vue
@@ -1,48 +1,45 @@
 <script lang="ts" setup>
-import type { ButtonGroupItem } from "~/types";
-import ButtonGroup              from "./base/ButtonGroup.vue";
+import type { ButtonGroupItem } from "~/types"
+import ButtonGroup from "./base/ButtonGroup.vue"
+import CopyButton from "./base/CopyButton.vue"
 
 const {
 	color = "green-400",
 	items = [
 		{
 			title: "DATA",
-			name: "data"
+			name: "data",
 		},
 		{
 			title: "HEADERS",
-			name: "headers"
-		}
+			name: "headers",
+		},
 	],
 	copyValue = "",
-	showSearch = false
+	showSearch = false,
 } = defineProps<{
 	color?: string
 	items?: ButtonGroupItem[]
 	copyValue?: string
 	showSearch?: boolean
-}>();
+}>()
 
 defineEmits<{
 	(e: "toggleSearch"): void
-}>();
+}>()
 
-const activeView = defineModel({ default: "data" });
+const activeView = defineModel({ default: "data" })
 </script>
 
 <template>
 	<div class="h-10 flex flex-shrink-0 items-center justify-between of-hidden border-b border-gray3 bg-gray1 px-2">
 		<div class="flex items-center of-hidden">
-			<ButtonGroup
-					v-model="activeView"
-					:items="items"
-					:active-color="color"
-					class="flex-shrink-0"
-			/>
-			<slot name="left"/>
+			<ButtonGroup v-model="activeView" :items="items" :active-color="color" class="flex-shrink-0" />
+			<slot name="left" />
 		</div>
 		<div class="flex flex-shrink-0 items-center of-hidden space-x-1">
-			<slot name="right"/>
+			<CopyButton :value="copyValue ? copyValue : ''" />
+			<slot name="right" />
 		</div>
 	</div>
 </template>
